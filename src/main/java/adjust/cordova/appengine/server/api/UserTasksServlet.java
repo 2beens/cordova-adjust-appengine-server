701bcaa9b3ada5e96be43bf11ca1056d6f15a7d0
serj <srdjan@adjust.com>
2017-12-24 14:53:55 +0100

    small change

--- src/main/java/adjust/cordova/appengine/server/api/UserTasksServlet.java
+++ src/main/java/adjust/cordova/appengine/server/api/UserTasksServlet.java
@@ -18,58 +18,62 @@ import adjust.cordova.appengine.server.services.UserTaskService;
 @WebServlet("/tasks")
 public class UserTasksServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserTasksServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserTasksServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String userName = request.getParameter("userName");
-		if(!UserService.exists(userName)) {
+		if (!UserService.exists(userName)) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
+
 		String text = request.getParameter("text");
-		if(text == null) {
+		if (text == null) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
+
 		UserTask newUserTask = new UserTask(userName, text);
 		UserTaskService.addNew(newUserTask);
-		
+
 		User user = UserService.get(userName);
 		user.addTask(newUserTask);
-		
+
 		response.getWriter().print("New task for user [" + userName + "] saved.");
 	}
-	
+
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String userName = req.getParameter("userName");
-		if(!UserService.exists(userName)) {
+		if (!UserService.exists(userName)) {
 			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
+
 		UserTaskService.removeAllUserTasks(userName);
-		
+
 		resp.getWriter().print("About to delete all tasks of [" + userName + "].");
 	}
 
